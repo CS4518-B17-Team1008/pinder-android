2b45f3304990c898d25cc16a1ba5715f5b70fbd8
Can Alper <calper.ql@gmail.com>
2017-12-12 17:21:25 -0500

    Primer for json over net

---
+++ app/src/main/java/team1008/b17/cs4518/wpi/pinderapp/request_handler/FirebaseRequestHandler.java
@@ -0,0 +1,24 @@
+package team1008.b17.cs4518.wpi.pinderapp.request_handler;
+
+import org.json.JSONObject;
+
+/**
+ * Created by calper on 12/12/17.
+ */
+
+public class FirebaseRequestHandler implements RequestHandler {
+
+    FirebaseRequestHandler(String address) {
+
+    }
+
+    @Override
+    public void send(JSONObject obj) {
+
+    }
+
+    @Override
+    public JSONObject recieve() {
+        return null;
+    }
+}
---
+++ app/src/main/java/team1008/b17/cs4518/wpi/pinderapp/request_handler/RequestHandler.java
@@ -0,0 +1,12 @@
+package team1008.b17.cs4518.wpi.pinderapp.request_handler;
+
+import org.json.JSONObject;
+
+/**
+ * Created by calper on 12/12/17.
+ */
+
+public interface RequestHandler {
+    public void send(JSONObject obj);
+    public JSONObject recieve();
+}
